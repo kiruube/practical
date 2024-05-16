51dba63686d334bb42643489ca320a376c62b697
Benjamin Kiruube Maranatha <benjaminkiruube@gmail.com>
2024-05-17 01:09:48 +0300

    Set up run time environment

---
+++ index.js
@@ -0,0 +1,15 @@
+//Dependencies
+const express = require('express')
+
+//Instantiation
+const app = express();
+const port = 3009;
+
+//Middleware
+
+//configuration
+
+//routes
+
+//bootstrapping
+app.listen(port, () => console.log(`Listening on ${port}`));
