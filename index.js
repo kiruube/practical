ce5208b4ffa69e315f5ed8b91f5dae7d760a228b
Benjamin Kiruube Maranatha <benjaminkiruube@gmail.com>
2024-05-17 01:39:51 +0300

    Set up database connection

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 //Dependencies
-const express = require('express')
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv') = config();
 //Instantiation
 const app = express();
 const port = 3009;
 
 //Middleware
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+});
+mongoose.connection
+.once('open', () => {
+    console.log('Mongoose connected Succesfully');
+})
+.on('error', err => {
+    console.log(`Connection error: ${err.message}`);
+});
 
 //configuration
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
 
 //routes
+
+//Invalid Routes
+app.get('*', (res,req) => {
+    res.send('404! This page does not exist..')
+});
 
 //bootstrapping
 app.listen(port, () => console.log(`Listening on ${port}`));
